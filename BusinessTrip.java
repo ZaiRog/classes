bf7b00c6c3004f9f47db2eb5f028dad36b7bcbda
ZaiRog <79278315+ZaiRog@users.noreply.github.com>
2021-11-23 01:41:16 +0300

    Update BusinessTrip.java

--- BusinessTrip.java
+++ BusinessTrip.java
@@ -1,31 +1,48 @@
-import by.gsu.epamlab.BusinessTrip;
+package by.gsu.epamlab;
 
-import static by.gsu.epamlab.BusinessTrip.convertingToRubles;
-
-public class Runner {
-    public static void main(String[] args) {
-        BusinessTrip[] businessTrips1 = new BusinessTrip[]{new BusinessTrip("Anton", 1000, 30),
-        new BusinessTrip("Egor", 1100, 21),
-        null, new BusinessTrip("Polina", 1050,19),
-        new BusinessTrip()};
-        int maxTotal = businessTrips1[0].getTotal();
-        for(BusinessTrip businessTrip2: businessTrips1){
-            if(businessTrip2 == null){
-                continue;
-            }
-            businessTrip2.show();
-            System.out.println();
-            if(businessTrip2.getTotal() > maxTotal){
-                maxTotal = businessTrip2.getTotal();
-            }
-        }
-        System.out.println("Max cost of business trip: " + convertingToRubles(maxTotal) + "\n");
-        businessTrips1[businessTrips1.length - 1].setTransportationExpenses(1001);
-        businessTrips1[businessTrips1.length - 1].show();
-        System.out.println("\nDuration of trips: " + (businessTrips1[0].getNumberOfDays()+businessTrips1[1].getNumberOfDays()));
-        for(BusinessTrip businessTrip: businessTrips1){
-            System.out.println(businessTrip);
-            System.out.println();
-        }
+public class BusinessTrip {
+    private final static int DAILY_ALLOWANCE_PART = 5000;
+    private String account;
+    private int transportationExpenses;
+    private int numberOfDays;
+    public BusinessTrip() {
+    }
+    public BusinessTrip(String account, int transportationExpenses, int numberOfDays) {
+        this.account = account;
+        this.transportationExpenses = transportationExpenses;
+        this.numberOfDays = numberOfDays;
+    }
+    public String getAccount() {
+        return account;
+    }
+    public void setAccount(String account) {
+        this.account = account;
+    }
+    public int getTransportationExpenses() {
+        return transportationExpenses;
+    }
+    public void setTransportationExpenses(int transportationExpenses) {
+        this.transportationExpenses = transportationExpenses;
+    }
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
+    public void setNumberOfDays(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+    public int getTotal(){
+        return (transportationExpenses+DAILY_ALLOWANCE_PART * numberOfDays);
+    }
+    public static String convertingToRubles(int pennyToConverting){
+        return String.format("%d.%02d", pennyToConverting / 100, pennyToConverting % 100);
+    }
+    public void show(){
+        System.out.printf("Employee`s account: %s \nTransportation expenses: %s \nNumber of days: %d \nTotal: %s\n",
+                account,convertingToRubles(transportationExpenses),numberOfDays,convertingToRubles(getTotal()));
+    }
+    @Override
+    public String toString() {
+        return String.format("%s;%s;%d;%s",
+                account,convertingToRubles(transportationExpenses), numberOfDays, convertingToRubles(getTotal()));
     }
 }
