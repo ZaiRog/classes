51791ab56c870aa098bb18273b048285f496b76a
ZaiRog <79278315+ZaiRog@users.noreply.github.com>
2021-11-22 23:44:00 +0300

    Update BusinessTrip.java

--- BusinessTrip.java
+++ BusinessTrip.java
@@ -14,43 +14,33 @@ public class BusinessTrip {
         this.transportationExpenses = transportationExpenses;
         this.numberOfDays = numberOfDays;
     }
-
-
     public String getAccount() {
         return account;
     }
-
     public void setAccount(String account) {
         this.account = account;
     }
-
     public int getTransportationExpenses() {
         return transportationExpenses;
     }
-
     public void setTransportationExpenses(int transportationExpenses) {
         this.transportationExpenses = transportationExpenses;
     }
-
     public int getNumberOfDays() {
         return numberOfDays;
     }
-
     public void setNumberOfDays(int numberOfDays) {
         this.numberOfDays = numberOfDays;
     }
-
     public int getTotal(){
-        return (transportationExpenses+DAILY_ALLOWANCE_PART*numberOfDays);
+        return transportationExpenses+DAILY_ALLOWANCE_PART*numberOfDays;
     }
-
     public void show(){
             System.out.println("Employee`s account: " + account +
                     "\nTransportation expenses: " + transportationExpenses/100+'.'+transportationExpenses%100+ " BYN" +
                     "\nNumber of days: " + numberOfDays +
                     "\nTotal: " + getTotal()/100+'.'+getTotal()%100 + " BYN");
     }
-
     @Override
     public String toString() {
         return "BusinessTrip:\n" +
