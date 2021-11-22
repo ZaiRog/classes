5147c918d09cc79343dd4124652799d78b1914d3
ZaiRog <79278315+ZaiRog@users.noreply.github.com>
2021-11-23 01:37:56 +0300

    Update BusinessTrip.java

--- BusinessTrip.java
+++ BusinessTrip.java
@@ -1,52 +1,31 @@
-package by.gsu.epamlab;
+import by.gsu.epamlab.BusinessTrip;
 
-public class BusinessTrip {
-    private final static int DAILY_ALLOWANCE_PART = 5000;
-    private String account;
-    private int transportationExpenses;
-    private int numberOfDays;
+import static by.gsu.epamlab.BusinessTrip.convertingToRubles;
 
-    public BusinessTrip() {
-    }
-
-    public BusinessTrip(String account, int transportationExpenses, int numberOfDays) {
-        this.account = account;
-        this.transportationExpenses = transportationExpenses;
-        this.numberOfDays = numberOfDays;
-    }
-    public String getAccount() {
-        return account;
-    }
-    public void setAccount(String account) {
-        this.account = account;
-    }
-    public int getTransportationExpenses() {
-        return transportationExpenses;
-    }
-    public void setTransportationExpenses(int transportationExpenses) {
-        this.transportationExpenses = transportationExpenses;
-    }
-    public int getNumberOfDays() {
-        return numberOfDays;
-    }
-    public void setNumberOfDays(int numberOfDays) {
-        this.numberOfDays = numberOfDays;
-    }
-    public int getTotal(){
-        return transportationExpenses+DAILY_ALLOWANCE_PART*numberOfDays;
-    }
-    public void show(){
-            System.out.println("Employee`s account: " + account +
-                    "\nTransportation expenses: " + transportationExpenses/100+'.'+transportationExpenses%100+ " BYN" +
-                    "\nNumber of days: " + numberOfDays +
-                    "\nTotal: " + getTotal()/100+'.'+getTotal()%100 + " BYN");
-    }
-    @Override
-    public String toString() {
-        return "BusinessTrip:\n" +
-                account + ';' +
-                transportationExpenses/100+'.'+transportationExpenses%100 + ';'+
-                numberOfDays + ';'+
-                getTotal()/100+'.'+getTotal()%100;
+public class Runner {
+    public static void main(String[] args) {
+        BusinessTrip[] businessTrips1 = new BusinessTrip[]{new BusinessTrip("Anton", 1000, 30),
+        new BusinessTrip("Egor", 1100, 21),
+        null, new BusinessTrip("Polina", 1050,19),
+        new BusinessTrip()};
+        int maxTotal = businessTrips1[0].getTotal();
+        for(BusinessTrip businessTrip2: businessTrips1){
+            if(businessTrip2 == null){
+                continue;
+            }
+            businessTrip2.show();
+            System.out.println();
+            if(businessTrip2.getTotal() > maxTotal){
+                maxTotal = businessTrip2.getTotal();
+            }
+        }
+        System.out.println("Max cost of business trip: " + convertingToRubles(maxTotal) + "\n");
+        businessTrips1[businessTrips1.length - 1].setTransportationExpenses(1001);
+        businessTrips1[businessTrips1.length - 1].show();
+        System.out.println("\nDuration of trips: " + (businessTrips1[0].getNumberOfDays()+businessTrips1[1].getNumberOfDays()));
+        for(BusinessTrip businessTrip: businessTrips1){
+            System.out.println(businessTrip);
+            System.out.println();
+        }
     }
 }
